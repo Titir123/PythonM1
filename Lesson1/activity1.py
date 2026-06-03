4c403a6cb17bd3368470a58656227005221a8466
Titir123 <suchishree0111@gmail.com>
2026-06-03 19:14:07 +0530

    first commit

---
+++ Lesson1/activity1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Suchishree.")
+
+# Printing a fun fact about myself
+
+print("I love watching movies.")
+
+# Performing and printing a simple calculation
+print("The result of 9 + 5 is:", 9 + 5)
---
+++ Lesson4/activity1.py
@@ -0,0 +1,14 @@
+# Storing Values
+marks1 = 98
+marks2 = 94
+marks3 = 41
+marks4 = 95
+marks5 = 11
+
+# Finding the total of trees
+sum = marks1+marks2+marks3+marks4+marks5
+print("the sum of all the 5 trees is: ", sum)
+
+# Finding the average of trees
+average = sum/5
+print("the average of all the tree is :", average)
