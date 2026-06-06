eb38f68740833fa281fb9596af0a5678d265de31
Titir123 <suchishree0111@gmail.com>
2026-06-06 17:28:44 +0530

    commit

---
+++ Lesson5/acp.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the temperature in °C: "))
+
+if temperature >= 25:
+    print("Suitable for wearing light and soft clothes.")
+else:
+    print("Weather is cool. Wear warm clothes.")
