c229e5142bee80d0b5487907c09a8c5251b840d1
Titir123 <suchishree0111@gmail.com>
2026-06-06 17:18:18 +0530

    2nd commit

---
+++ Lesson2/acp.py
@@ -0,0 +1,11 @@
+birthday1 = "12-01-2000"
+birthday2 = "25-03-1998"
+birthday3 = "07-07-2001"
+birthday4 = "18-09-1999"
+birthday5 = "30-12-2002"
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
