20026394f9e213526e456f296e8ca50b0ca6056c
Titir123 <suchishree0111@gmail.com>
2026-06-06 17:33:18 +0530

    commit

---
+++ Lesson6/acp.py
@@ -0,0 +1,6 @@
+ch = input("Enter a character: ")
+
+if ch in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    print("The entered character is an alphabet.")
+else:
+    print("The entered character is not an alphabet.")
